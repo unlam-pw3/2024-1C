a1b84016e28c407b006dcb5583cb031675c0eabd
fabio <fabiiakd1@gmail.com>
2024-04-03 18:15:05 -0300

    juego tateti

---
+++ Ta-te-ti Game/Ta-te-ti Game/ConsoleInput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ta_te_ti_Game
+{
+    public interface IConsoleInput {
+        public String ReadLine();
+    }
+
+    public class ConsoleInput : IConsoleInput
+    {
+        public string ReadLine() => Console.ReadLine();
+    }
+}
---
+++ Ta-te-ti Game/Ta-te-ti Game/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Ta_te_ti_Game;
+using Ta_te_tiGame.UnitTests;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var consoleInput = new ConsoleInput();
+        var consoleOutput = new ConsoleOutput();
+
+        var game = new TatetiGame(consoleInput, consoleOutput);
+
+        game.StartGame();
+
+        Console.WriteLine("Presione cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
