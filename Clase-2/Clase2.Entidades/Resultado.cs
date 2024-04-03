a696deecca21ea039c948a35d540a93e5bf32c0c
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2024-04-03 20:57:45 -0300

    agregando ejemplo de webapi resultados y consumiendola desde el winform

---
+++ Clase-2/Clase2.Entidades/Resultado.cs
@@ -0,0 +1,9 @@
+﻿namespace Clase2.Entidades;
+
+public class Resultado
+{
+    public string EquipoLocal { get; set; }
+    public string EquipoVisitante { get; set; }
+    public string GolesLocal { get; set; }
+    public string GolesVisitante { get; set; }
+}
