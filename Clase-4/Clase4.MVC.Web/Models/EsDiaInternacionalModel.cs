04009e0a29a4facdccb0ddfe700885c4aaa06924
eliromero2 <elianaromero002@gmail.com>
2024-04-23 21:55:41 -0300

    Agregue esDiaInternacional con parametros de dia y mes

---
+++ Clase-4/Clase4.MVC.Web/Models/EsDiaInternacionalModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Clase4.MVC.Web.Models
+{
+    public class EsDiaInternacionalModel
+    {
+        public int Dia { get; set; }
+        public int Mes { get; set; }
+        public string Nombre { get; set; }
+        public bool EsDiaInternacional { get; set; }
+    }
+}
--- Clase-4/Clase4.MVC.Web/Models/EsFeriadoModel.cs
+++ Clase-4/Clase4.MVC.Web/Models/EsFeriadoModel.cs
@@ -4,5 +4,6 @@ public class EsFeriadoModel
 {
     public int Dia { get; set; }
     public int Mes { get; set; }
+
     public bool EsFeriado { get; set; }
 }
