ef7ad276a0bd678768f5394d7a66c83721903a7a
Emiliano R <emiliano.r.javier@gmail.com>
2024-05-08 21:24:19 -0300

    no message

--- Clase-4/Clase4.MVC.Web/Models/DiaEspecialModel.cs
+++ Clase-4/Clase4.MVC.Web/Models/DiaEspecialModel.cs
@@ -6,13 +6,8 @@ public class DiaEspecialModel
     [Required(ErrorMessage = "¡La fecha es requerida!")]
     [DataType(DataType.Date, ErrorMessage="La fecha ingresada no está en un formato válido")]
     public DateTime Fecha { get; set; }
-
     [Required(ErrorMessage = "¡El nombre es requerido!")]
     [StringLength(50, ErrorMessage = "¡El nombre no puede superar los 50 caracteres!")]
-    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "El nombre solo puede contener letras y espacios")]
     public string Nombre { get; set; }
-
-    [StringLength(150, ErrorMessage = "La descripción no puede superar los 200 caracteres")]
-    [Required(ErrorMessage = "¡La descripcion es requerida!")]
-    public string Descripcion { get; set; }
+    public string? Descripcion { get; set; }
 }
