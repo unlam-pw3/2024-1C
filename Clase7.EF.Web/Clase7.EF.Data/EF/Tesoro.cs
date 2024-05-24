002a40e7d59783a63b732df39b6f66fe6801c564
gabrieldp6880 <ivandp6880@gmail.com>
2024-05-24 17:28:16 -0300

    Agregar Ubicacion con scripts y logica

---
+++ Clase7.EF.Web/Clase7.EF.Data/EF/Tesoro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Clase7.EF.Data.EF;
+
+public partial class Tesoro
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string? ImagenRuta { get; set; }
+}
---
+++ Clase7.EF.Web/Clase7.EF.Web/Program.cs
@@ -0,0 +1,32 @@
+using Clase7.EF.Data.EF;
+using Clase7.EF.Logica;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<Pw31cIslaTesoroContext>();
+builder.Services.AddScoped<ITesoroServicio, TesoroServicio>();
+builder.Services.AddScoped<IUbicacionServicio, UbicacionServicio>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
