110fa61a059d2c5c8609789ea15aae96ab950675
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2024-06-05 20:34:19 -0300

    agregando resolucion modelo parcial 2

---
+++ Clase-Modelo-2doParcial/Modelo2doParcial/Modelo2doParcial.Entidades/EF/Sucursal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Modelo2doParcial.Entidades.EF
+{
+    public partial class Sucursal
+    {
+        public Sucursal()
+        {
+            Empleados = new HashSet<Empleado>();
+        }
+
+        public int IdSucursal { get; set; }
+        public string Direccion { get; set; } = null!;
+        public bool Eliminada { get; set; }
+
+        public virtual ICollection<Empleado> Empleados { get; set; }
+    }
+}
